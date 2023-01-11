08fb28b8e3bc5c6527489c543d8139ef9e226f5b
RichardReh <richardreh@gmx.de>
2023-01-11 18:31:04 +0100

    Ordnerstruktur angepasst + Test CSS erstellt

--- Rapid_Prototype/index.js
+++ Rapid_Prototype/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express()
 
+app.use(express.static('public'))
 
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname +"/index.html")
+    res.sendFile(__dirname +"/views/index.html")
 })
 
 const port = process.env.PORT || 8002
